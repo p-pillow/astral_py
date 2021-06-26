d43e333b84e7a9db38174642e85879663a2bf730
Andrey Z <ppillowrepo@gmail.com>
2021-06-26 14:50:59 +0300

    Effects update

--- Objects/Effects.py
+++ Objects/Effects.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Description of all effects
+    title - a title of an effect
+    is_clearable - can this effect be cleared by simple clear
+    type - buff or debuff
+    reaction_time - it can work before a round, with action or after a round
+"""
+
 ALL_EFFECTS = {
-    1: {"is_cleanable": True, "duration": 1, "damage": 2}
+    "mana_resist": {"is_clearable": True, "type": "buff"  , "reaction_time": 0},
+    "prophecy": {"is_clearable": True, "type": "buff"  , "reaction_time": 0},
+    "falling": {"is_clearable": True, "type": "debuff", "reaction_time": 0},
+    "levitate": {"is_clearable": True, "type": "debuff", "reaction_time": 0},
+    "damage_resist": {"is_clearable": True, "type": "debuff", "reaction_time": 0},
 }
